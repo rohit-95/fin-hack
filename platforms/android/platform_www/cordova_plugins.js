cff801770ad831ad387b76b13da0788d3f291b07
Rohit Gohri <gohri.rohit@gmail.com>
2016-02-13 18:55:36 +0530

    removed income flow redirect loop

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,9 +1,23 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/cordova-plugin-mauron85-background-geolocation/www/backgroundGeoLocation.js",
+        "id": "cordova-plugin-mauron85-background-geolocation.backgroundGeoLocation",
+        "clobbers": [
+            "backgroundGeoLocation"
+        ]
+    },
+    {
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/phonegap-plugin-push/www/push.js",
+        "id": "phonegap-plugin-push.PushNotification",
+        "clobbers": [
+            "PushNotification"
+        ]
     }
 ];
 module.exports.metadata = 
--- www/js/common.js
+++ www/js/common.js
@@ -7,6 +7,10 @@ var _currPageName = "";
 var _windowHeight = $(window).height();
 
 $(document).ready(function () {
+    var input = $("<input>")
+        .attr("type", "hidden")
+        .attr("name", "userNo").val(_userDetails.userNo);
+
     $(".button-collapse").sideNav();
     console.log("ready");
 
@@ -31,8 +35,9 @@ $(document).ready(function () {
             }
         );
     $('#addExpenseForm')
+        .append($(input))
         .ajaxForm({
-                url : _apiBaseUrl + 'addExpense',
+                url : _apiBaseUrl + 'transaction/new',
                 type: 'post',
                 success : function(response) {
                     console.log(response);
@@ -41,8 +46,9 @@ $(document).ready(function () {
             }
         );
     $('#addDepositForm')
+        .append($(input))
         .ajaxForm({
-            url : _apiBaseUrl + 'addDeposit',
+            url : _apiBaseUrl + 'transaction/new',
             type: 'post',
             success : function(response) {
                 console.log(response);
@@ -51,8 +57,9 @@ $(document).ready(function () {
         }
         );
     $('#incomeFlow')
+        .append($(input))
         .ajaxForm({
-                url : _apiBaseUrl + 'incomeFlow',
+                url : _apiBaseUrl + 'start',
                 type: 'post',
                 success : function(response) {
                     console.log(response);
@@ -64,3 +71,44 @@ $(document).ready(function () {
 
 
 document.addEventListener("deviceready", onDeviceReady, false);
+
+
+function onDeviceReady () {
+
+    /**
+     * This callback will be executed every time a geolocation is recorded in the background.
+     */
+    var callbackFn = function(location) {
+        console.log('[js] BackgroundGeoLocation callback:  ' + location.latitude + ',' + location.longitude);
+
+        // Do your HTTP request here to POST location to your server.
+        jQuery.post(url, JSON.stringify(location));
+
+        /*
+         IMPORTANT:  You must execute the finish method here to inform the native plugin that you're finished,
+         and the background-task may be completed.  You must do this regardless if your HTTP request is successful or not.
+         IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
+         */
+        backgroundGeoLocation.finish();
+    };
+
+    var failureFn = function(error) {
+        console.log('BackgroundGeoLocation error');
+    };
+
+    // BackgroundGeoLocation is highly configurable. See platform specific configuration options
+    backgroundGeoLocation.configure(callbackFn, failureFn, {
+        desiredAccuracy: 10,
+        stationaryRadius: 20,
+        distanceFilter: 30,
+        debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
+        stopOnTerminate: false, // <-- enable this to clear background location settings when the app terminates
+        interval: 30000
+    });
+
+    // Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app.
+    backgroundGeoLocation.start();
+
+    // If you wish to turn OFF background-tracking, call the #stop method.
+    // backgroundGeoLocation.stop();
+}
